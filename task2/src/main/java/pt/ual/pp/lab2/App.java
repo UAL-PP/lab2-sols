300f1b77d35a6d0b3ebafb16b622e0dd7ede114e
amgs <undisclosed>
2021-04-19 16:45:55 +0100

    Task2: a) done.

--- task2/src/main/java/pt/ual/pp/lab2/App.java
+++ task2/src/main/java/pt/ual/pp/lab2/App.java
@@ -18,7 +18,7 @@ class Teller {
     public void run() {
         try {
             int numWaitSeconds = Math.min(5, random.nextInt(this.maximumWaitSeconds) + this.minimumWaitSeconds);
-            System.out.println("Teller "+this.id+" waiting for "+numWaitSeconds+" seconds.");
+            System.out.print("Teller "+this.id+" waiting for "+numWaitSeconds+" seconds... ");
             Thread.sleep(1000 * numWaitSeconds);
         } catch(InterruptedException e) {
             e.printStackTrace();
@@ -31,17 +31,14 @@ public class App {
     public static void main(String[] args) throws InterruptedException {
         Random random = new Random();
         Teller teller1 = new Teller(1, 1, 5, random);
+        Teller teller2 = new Teller(2, 3, 8, random);
+        Teller teller3 = new Teller(3, 2, 7, random);
 
         while(true) {
             //Calcular o tempo de espera para cada caixa: 1-5, 3-8, 2-7
-
             teller1.run();
-
-            Thread.sleep(1000 * Math.min(8, random.nextInt(8) + 3));
-            System.out.println("Client on teller 2.");
-
-            Thread.sleep(1000 * Math.min(7, random.nextInt(7) + 2));
-            System.out.println("Client on teller 3.");
+            teller2.run();
+            teller3.run();
         }
     }
 }
